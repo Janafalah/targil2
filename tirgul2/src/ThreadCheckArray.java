e4e66225d1af173f4c22b598e828ab33ebd1bc99
pc <pc@10.10.2.226>
2025-03-23 01:55:22 -0700

    12

--- tirgul2/src/ThreadCheckArray.java
+++ tirgul2/src/ThreadCheckArray.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
+
 public class ThreadCheckArray implements Runnable 
 {
 	private boolean flag;
 	private boolean [] winArray;
 	SharedData sd;
-	int[] array;
+	ArrayList<Integer> array;
 	int b;
 	
 	public ThreadCheckArray(SharedData sd) 
@@ -11,10 +13,10 @@ public class ThreadCheckArray implements Runnable
 		this.sd = sd;	
 		synchronized (sd) 
 		{
-			array = sd.getArray();
+			array = new ArrayList<>(sd.getArray());
 			b = sd.getB();
 		}		
-		winArray = new boolean[array.length];
+		winArray = new boolean[array.size()];
 	}
 	
 	void rec(int n, int b)
@@ -26,7 +28,7 @@ public class ThreadCheckArray implements Runnable
 		}	
 		if (n == 1)
 		{
-			if(b == 0 || b == array[n-1])
+			if(b == 0 || b == array.get(n-1))
 			{
 				flag = true;
 				synchronized (sd) 
@@ -34,12 +36,12 @@ public class ThreadCheckArray implements Runnable
 					sd.setFlag(true);
 				}			
 			}
-			if (b == array[n-1])
+			if (b == array.get(n-1))
 				winArray[n-1] = true;
 			return;
 		}
 		
-		rec(n-1, b - array[n-1]);
+		rec(n-1, b - array.get(n-1));
 		if (flag)
 			winArray[n-1] = true;
 		synchronized (sd) 
@@ -49,31 +51,41 @@ public class ThreadCheckArray implements Runnable
 		}	
 		rec(n-1, b);
 	}
+	public void run() 
+    {
+        int arraySize = array.size();
+        
+        if (arraySize == 1) 
+        {
+            if (b == array.get(0) && !flag)
+            {
+                winArray[0] = true;
+                flag = true;
+                synchronized (sd) 
+                {
+                    sd.setFlag(true);
+                }
+            }
+        } 
+        else 
+        {
+            if (Thread.currentThread().getName().equals("thread1"))
+                rec(arraySize - 1, b - array.get(arraySize - 1));
+            else 
+                rec(arraySize - 1, b);
+        }
+        
+        if (flag)
+        {
+            if (Thread.currentThread().getName().equals("thread1"))
+                winArray[arraySize - 1] = true;
+            
+            synchronized (sd) 
+            {
+                sd.setWinArray(winArray);
+            }    
+        }
+    }
 
-	public void run() {
-		if (array.length != 1)
-			if (Thread.currentThread().getName().equals("thread1"))
-				rec(array.length-1, b - array[array.length - 1]);
-			else 
-				rec(array.length-1, b);
-		if (array.length == 1)
-			if (b == array[0] && !flag)
-			{
-				winArray[0] = true;
-				flag = true;
-				synchronized (sd) 
-				{
-					sd.setFlag(true);
-				}
-			}
-		if (flag)
-		{
-			if (Thread.currentThread().getName().equals("thread1"))
-				winArray[array.length - 1] = true;
-			synchronized (sd) 
-			{
-				sd.setWinArray(winArray);
-			}	
-		}
-	}
+
 }
